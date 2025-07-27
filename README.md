b24896b928f224522d2a4b2ddc728501850c411c
BlessmanJay <blessmanjay@gmail.com>
2025-07-27 22:05:26 +0100

    Set up Next.js Project

--- README.md
+++
@@ -1,3 +0,0 @@
-# Next.js Project Setup and Basics
-
-This repository serves as the foundational setup for building a modern web application using Next.js, TypeScript, and Tailwind CSS. It guides developers through the process of creating reusable components, implementing routing, fetching external data, and following best practices for project structure and performance.
---
+++ alx-project-0x02/alx-project-2/pages/index.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Welcome | ALX Project</title>
+      </Head>
+      <main className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Welcome to ALX Project 2 with Next.js, TypeScript & Tailwind!
+        </h1>
+      </main>
+    </>
+  );
+}
