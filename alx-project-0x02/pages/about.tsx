d1ead8871b0b47228522c89fe51de46e3256ce4f
BlessmanJay <blessmanjay@gmail.com>
2025-07-27 23:20:09 +0100

    Create and Use a Button Component

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Button from "@/components/common/Button";
 
 export default function AboutPage() {
   return (
@@ -6,10 +7,15 @@ export default function AboutPage() {
       <Head>
         <title>About Page</title>
       </Head>
-      <main className="flex items-center justify-center min-h-screen bg-yellow-100">
-        <h1 className="text-4xl font-bold text-yellow-800">
+
+      <main className="flex flex-col items-center justify-center gap-4 min-h-screen bg-yellow-100">
+        <h1 className="text-4xl font-bold text-yellow-800 mb-6">
           About This Project
         </h1>
+
+        <Button label="Small Button" size="small" shape="rounded-sm" />
+        <Button label="Medium Button" size="medium" shape="rounded-md" />
+        <Button label="Large Button" size="large" shape="rounded-full" />
       </main>
     </>
   );
